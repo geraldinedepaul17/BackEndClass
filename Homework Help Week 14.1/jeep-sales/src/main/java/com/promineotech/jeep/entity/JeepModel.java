82ce144877ab6b59dd0051c33d54bfcedec74b8e
Geraldine C <107955441+geraldinedepaul17@users.noreply.github.com>
2022-09-28 14:01:28 -0500

    Homework Help Week 14.1
    Log works but now the test is not clear

---
+++ Homework Help Week 14.1/jeep-sales/src/main/java/com/promineotech/jeep/entity/JeepModel.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.promineotech.jeep.entity;
+
+/**
+ * @author gerriciancanelli
+ *
+ */
+public enum JeepModel {
+	GRAND_CHEROKEE,
+	CHEROKEE,
+	COMPASS,
+	RENEGADE,
+	WRANGLER,
+	GLADIATOR, 
+	WRANGLER_4XE
+	
+}
